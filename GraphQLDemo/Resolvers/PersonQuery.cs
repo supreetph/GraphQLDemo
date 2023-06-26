b965c7bcce8d069f4853040d7b362b89361c20a9
Supreet Prakash Hadagali <s.prakash.hadagali@accenture.com>
2023-06-26 15:24:48 +0530

    Add project files.

---
+++ GraphQLDemo/Resolvers/PersonQuery.cs
@@ -0,0 +1,15 @@
+﻿using GraphQLDemo.Models;
+
+namespace GraphQLDemo.Resolvers
+{
+    public class PersonQuery
+    {
+        private readonly List<Person> _persons = new List<Person>
+    {
+        new Person { Id = 1, Name = "John", Age = 25 },
+        new Person { Id = 2, Name = "Jane", Age = 30 }
+    };
+
+        public List<Person> GetPersons() => _persons;
+    }
+}
